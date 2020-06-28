65688356b20cf45dbaffcf8812c902b60940b72e
carlos-reiss <carlosreis10.06@hotmail.com>
2020-06-28 11:00:00 -0300

    aprendendo sobre testes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,19 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/fakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('Create Appointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '11242',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('11242');
+  });
+  it('should not be able to create to appointment on the same time', () => {});
 });
