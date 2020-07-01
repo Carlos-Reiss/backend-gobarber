d6af7ab468163a2ab3c0a3c3bd0686a791ea226b
carlos-reiss <carlosreis10.06@hotmail.com>
2020-06-30 23:23:14 -0300

    update teste authenticate

--- src/modules/users/services/AuthenticatedUserServices.spec.ts
+++ src/modules/users/services/AuthenticatedUserServices.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/fakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
@@ -18,7 +19,7 @@ describe('Authenticate User', () => {
       fakeHashProvider
     );
 
-    await createUser.execute({
+    const user = await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
@@ -30,5 +31,49 @@ describe('Authenticate User', () => {
     });
 
     expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticatedUser = new AuthenticatedUserServices(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+
+    expect(
+      authenticatedUser.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticatedUser = new AuthenticatedUserServices(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+    const createUser = new CreatedUserService(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticatedUser.execute({
+        email: 'johndoe@example.com',
+        password: 'wrong-password',
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
