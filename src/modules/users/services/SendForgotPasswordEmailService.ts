330f602be09561fca65efb95639136b9863e938b
carlos-reiss <carlosreis10.06@hotmail.com>
2020-07-08 12:58:49 -0300

    template de emails'

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,4 @@
 import IUsersTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-/* import User from '@modules/users/infra/typeorm/entities/user'; */
 import AppError from '@shared/errors/AppError';
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
@@ -30,10 +29,20 @@ class SendForgotPasswordEmailService {
     }
 
     const { token } = await this.userTokensRepository.generate(user.id);
-    await this.mailProvider.sendMail(
-      email,
-      ` Pedido de recuperação de senha recebido: ${token} `
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber]Recuperação de senha.',
+      templateData: {
+        template: 'Olá, {{name}}! seu token: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 export default SendForgotPasswordEmailService;
